b29887c313b6d7443b1e183e94ded5d062f1034b
brayanbarroso <brayanbarroso01@gmail.com>
2026-02-06 23:12:49 -0500

    Modificando ruta del frontend

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
-import express from 'express';
-import cors from 'cors';
-import bodyParser from 'body-parser';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cors from "cors";
+import bodyParser from "body-parser";
+import path from "path";
+import { fileURLToPath } from "url";
 
-import { initDB } from './config/db.js';
-import apiRoutes from './routes/api.js';
+import { initDB } from "./config/db.js";
+import apiRoutes from "./routes/api.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -19,16 +19,25 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Servir archivos estáticos del frontend (opcional)
-app.use(express.static(path.join(__dirname, '../frontend')));
+//app.use(express.static(path.join(__dirname, '../frontend')));
+app.use(
+  express.static(path.join(__dirname, "https://virtual-rifa.vercel.app/")),
+);
 
 // Ruta raíz
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+// app.get('/', (req, res) => {
+//   res.sendFile(path.join(__dirname, '../frontend/index.html'));
+// });
+
+app.get("/", (req, res) => {
+  res.sendFile(
+    path.join(__dirname, "https://virtual-rifa.vercel.app/index.html"),
+  );
 });
 
 // Iniciar servidor tras inicializar la DB y cargar rutas modulares
 initDB().then(() => {
-  app.use('/api', apiRoutes);
+  app.use("/api", apiRoutes);
 
   app.listen(PORT, () => {
     console.log(`\n🚀 Servidor corriendo en http://localhost:${PORT}`);
@@ -36,6 +45,6 @@ initDB().then(() => {
   });
 });
 
-process.on('unhandledRejection', (error) => {
-  console.error('Error no manejado:', error);
+process.on("unhandledRejection", (error) => {
+  console.error("Error no manejado:", error);
 });
